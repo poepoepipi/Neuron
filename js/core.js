96e4aa52e7d229579303442bcec24b89011b7b86
SOEP <139007040+poepoepipi@users.noreply.github.com>
2026-06-29 10:22:51 +0200

    new file:   chat.html 	new file:   css/base.css 	modified:   index.html     new file:   js/core.js 	deleted:    script.js 	deleted:    style.css

---
+++ js/core.js
@@ -0,0 +1,12 @@
+// base
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website loaded!");
+
+    const button = document.getElementById("testButton");
+
+    button.addEventListener("click", () => {
+        alert("Button clicked!");
+    });
+});
+
+//code
